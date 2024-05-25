ad289e4863b4dae196af848130b0b8496808a688
Kelly Martina <kellymartu@hotmail.com>
2024-05-24 21:27:40 -0300

    adicionar transformacao com pandas

---
+++ src/transformacao/main.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import sqlite3
+import pandas as pd
+
+
+# Definir o caminho para o arquivo JSONl
+df = pd.read_json('../data/data.jsonl', lines=True)
+
+# Setar o pandas para mostrar todas as colunas
+pd.options.display.max_columns = None
+
+# Adicionar colunas
+df['_source'] = "https://lista.mercadolivre.com.br/tenis-de-corrida-feminino"
+df['_data_coleta'] = datetime.now()
+
+# Tratar os valores nulos para colunas númericas e de texto
+df['old_price_reais'] = df['old_price_reais'].fillna(0).astype(float)
+df['old_price_centavos'] = df['old_price_centavos'].fillna(0).astype(float)
+df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
+df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
+df['reviews_rating_number'] = df['reviews_rating_number'].fillna(0).astype(float)
+
+# Remover () do `reviews_amount`
+df['reviews_amount'] = df['reviews_amount'].str.replace('[\(\)]', '', regex=True)
+df['reviews_amount'] = df['reviews_amount'].fillna(0).astype(int)
+
+# Tratar os precos como floats e calcular os valores totais
+df['old_price'] = df['old_price_reais'] + df['old_price_centavos'] / 100
+df['new_price'] = df['new_price_reais'] + df['new_price_centavos'] / 100
+
+# Remover as colunas antigas de precos
+df.drop(columns=['old_price_reais', 'old_price_centavos', 'new_price_reais', 'new_price_centavos'], inplace=True)
+
+# Conectar ao banco de dados SQLite
+conn = sqlite3.connect('../data/quotes.db')
+
+# Salvar o DataFrame no banco de dados SQLite
+df.to_sql('mercadolivre_items', conn, if_exists='replace', index=False)
+
+# Fechar a conexão com o banco de dados
+conn.close()
