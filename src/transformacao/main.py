da85facb54adf69ac520b022a96849eb1f7c4430
Kelly Martina <kellymartu@hotmail.com>
2024-05-24 23:20:18 -0300

    dashboard e ajustes

--- src/transformacao/main.py
+++ src/transformacao/main.py
@@ -17,16 +17,18 @@ df['_data_coleta'] = datetime.now()
 df['old_price_reais'] = df['old_price_reais'].fillna(0).astype(float)
 df['old_price_centavos'] = df['old_price_centavos'].fillna(0).astype(float)
 df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
-df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
+df['new_price_centavos'] = df['new_price_centavos'].fillna(0).astype(float)
 df['reviews_rating_number'] = df['reviews_rating_number'].fillna(0).astype(float)
 
 # Remover () do `reviews_amount`
 df['reviews_amount'] = df['reviews_amount'].str.replace('[\(\)]', '', regex=True)
 df['reviews_amount'] = df['reviews_amount'].fillna(0).astype(int)
 
+
 # Tratar os precos como floats e calcular os valores totais
 df['old_price'] = df['old_price_reais'] + df['old_price_centavos'] / 100
 df['new_price'] = df['new_price_reais'] + df['new_price_centavos'] / 100
+
 
 # Remover as colunas antigas de precos
 df.drop(columns=['old_price_reais', 'old_price_centavos', 'new_price_reais', 'new_price_centavos'], inplace=True)
